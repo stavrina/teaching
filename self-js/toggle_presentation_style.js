bc46af10d2a5b40f9f706d1e373d24625b7f4671
Stavrina <16238160+stavrina@users.noreply.github.com>
2023-01-30 18:34:20 +0000

    Add semicolons

--- self-js/toggle_presentation_style.js
+++ self-js/toggle_presentation_style.js
@@ -8,12 +8,12 @@ $(document).ready(function() {
     var style3 = document.getElementById("black-style");
     var style4 = document.getElementById("dracula-style");
     var style5 = document.getElementById("moon-style");
-    var style6 = document.getElementById("white-style")
+    var style6 = document.getElementById("white-style");
 
-    style1.onclick = function() { swapStyleSheet("dist/theme/beige.css") }
-    style2.onclick = function() { swapStyleSheet("dist/theme/simple.css") }
-    style3.onclick = function() { swapStyleSheet("dist/theme/black.css") }
-    style4.onclick = function() { swapStyleSheet("dist/theme/dracula.css") }
-    style5.onclick = function() { swapStyleSheet("dist/theme/moon.css") }
-    style6.onclick = function() { swapStyleSheet("dist/theme/white.css") }
+    style1.onclick = function() { swapStyleSheet("dist/theme/beige.css"); }
+    style2.onclick = function() { swapStyleSheet("dist/theme/simple.css"); }
+    style3.onclick = function() { swapStyleSheet("dist/theme/black.css"); }
+    style4.onclick = function() { swapStyleSheet("dist/theme/dracula.css"); }
+    style5.onclick = function() { swapStyleSheet("dist/theme/moon.css"); }
+    style6.onclick = function() { swapStyleSheet("dist/theme/white.css"); }
 });
